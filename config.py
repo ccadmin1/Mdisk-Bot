f631a3aeea152519c8950f62354de8bf34b7a09c
WARRIORS <56763972+ccadmin1@users.noreply.github.com>
2022-02-01 22:17:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("5159393792:AAEkeGYIvbR7YSk0vt_ycXZldwrvIyk8tpQ", "")
 
-    API_ID = int(os.environ.get("API_ID", 123456))
+    API_ID = int(os.environ.get("9244035", 123456))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("66c62a85cfbf593a991ea680223a0549", "")
     
-    API_KEY = os.environ.get("API_KEY", "")
+    API_KEY = os.environ.get("mx1Xtbpav1m18HUn4hWp", "")
 
     # AUTH_USERS = set(str(x) for x in os.environ.get("AUTH_USERS", "").split())
 
